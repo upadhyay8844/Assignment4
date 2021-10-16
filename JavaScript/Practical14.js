05517e5ca8fa4b00e798da1ac95e0216780f2362
upadhyay8844 <bhavya.upadhyay_cs19@gla.ac.in>
2021-10-16 10:16:00 +0530

    Add Practical14

--- JavaScript/Practical14.js
+++ JavaScript/Practical14.js
@@ -1,26 +1,24 @@
-let count = 0
+let count = 0;
 let x = document.getElementById("counter");
 let minusButton = document.getElementById("minus");
 minusButton.style.color = "gray";
 minusButton.style.border = "gray";
 
 function increment() {
-    count++;
-    x.innerHTML = count;
-    minusButton.style.color = "black";
-    minusButton.style.border = ""; 
+  count++;
+  x.innerHTML = count;
+  minusButton.style.color = "black";
+  minusButton.style.border = "";
 }
 
 function decrement() {
-    if (count==1) {
-        count--;
-        x.innerHTML = count;
-        minusButton.style.color = "gray";
-        minusButton.style.border = "gray";
-    }
-    else if(count>=2) {
+  if (count == 1) {
     count--;
     x.innerHTML = count;
-    }
-    
+    minusButton.style.color = "gray";
+    minusButton.style.border = "gray";
+  } else if (count >= 2) {
+    count--;
+    x.innerHTML = count;
+  }
 }
